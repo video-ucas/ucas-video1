31d80f6e9caa8305279da56dc907910b5cbc489f
sugarlwmons <1459091816@qq.com>
2021-01-07 21:05:56 +0800

    添加视频库

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Video.create(name:'火影忍者',video_url:'/static/火影忍者.mp4',picture_url:'火影忍者',count:0)
-Video.create(name:'千本樱： 古筝翻奏 --- 墨韵随步摇',video_url:'/static/qbl.mp4',picture_url:'qbl',count:0)
+Video.create(name:'本地视频',video_url:'/static/本地视频.mp4',picture_url:'本地视频',count:0)
+Video.create(name:'王冰冰的vlog',video_url:'/static/王冰冰vlog.mp4',picture_url:'王冰冰vlog',count:0)
+Video.create(name:'送给男票的小礼物',video_url:'/static/送给男票的小礼物.mp4',picture_url:'送给男票的小礼物',count:0)
+Video.create(name:'从大学到毕业的恋爱同居生活',video_url:'/static/从大学到毕业的恋爱同居生活.mp4',picture_url:'从大学到毕业的恋爱同居生活',count:0)
+Video.create(name:'周杰伦-晴天MV',video_url:'/static/周杰伦-晴天MV.mp4',picture_url:'周杰伦-晴天MV',count:0)
