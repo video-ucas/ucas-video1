9fe8b611ddfe0ed5ecac89eb9a4ef2d0e196b5dc
wuyurong <yuk_wing@163.com>
2021-01-07 19:47:32 +0800

    上传视频

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Video.create(name:'火影忍者',video_url:'/static/火影忍者.mp4',picture_url:'火影忍者',count:0)
 Video.create(name:'千本樱： 古筝翻奏 --- 墨韵随步摇',video_url:'/static/qbl.mp4',picture_url:'qbl',count:0)
+Video.create(name:'定日镜——未来的太阳能？',video_url:'/static/定日镜.mp4',picture_url:'定日镜',count:0)
+Video.create(name:'在资本面前，底层人民算什么？',video_url:'/static/资本与法治.mp4',picture_url:'资本与法治',count:0)
+Video.create(name:'procreate',video_url:'/static/procreate.mp4',picture_url:'procreate',count:0)
+Video.create(name:'2020，一部西方价值观在中国的败亡史',video_url:'/static/西方价值观的败亡.mp4',picture_url:'西方价值观的败亡',count:0)
