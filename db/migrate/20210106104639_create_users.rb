1a732dbc2cb46943519dfbd30d3477181471e198
sugarlwmons <1459091816@qq.com>
2021-01-07 22:06:58 +0800

    增加上传视频校验

--- db/migrate/20210106104639_create_users.rb
+++ db/migrate/20210106104639_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :password_digest
-
+      t.boolean :admin
       t.timestamps
     end
   end
