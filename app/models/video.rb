972b34e310ebccec3bc9ad2dd23669f2757699b5
sugarlwmons <1459091816@qq.com>
2021-01-10 20:01:07 +0800

    add contents to video.rb

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 #1.5 wzm
 class Video < ApplicationRecord
-  validates :name, presence: true,uniqueness: true
-  validates :video_url, presence: true,uniqueness: true
-  validates :picture_url, presence: true,uniqueness: true
+  validates :name, presence: true,uniqueness: true  #确保视频名不重复
+  validates :video_url, presence: true,uniqueness: true  #确保视频链接唯一
+  validates :picture_url, presence: true,uniqueness: true  #确保封面链接唯一
 end
