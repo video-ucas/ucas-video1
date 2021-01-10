c4c39ab0f0ecf829dc5599ab96fb06ed3aa0dbe3
sugarlwmons <1459091816@qq.com>
2021-01-10 20:05:16 +0800

    add contents to create_videos.rb

--- db/migrate/20210106074208_create_videos.rb
+++ db/migrate/20210106074208_create_videos.rb
@@ -1,10 +1,10 @@
 class CreateVideos < ActiveRecord::Migration[5.1]
   def change
     create_table :videos do |t|
-      t.string :name, unique: true
-      t.string :video_url, unique: true
-      t.string :picture_url, unique: true
-      t.integer :count
+      t.string :name, unique: true  #视频名，唯一
+      t.string :video_url, unique: true  #视频链接，唯一
+      t.string :picture_url, unique: true  #封面链接，唯一
+      t.integer :count  #观看次数
       t.timestamps
     end
   end
